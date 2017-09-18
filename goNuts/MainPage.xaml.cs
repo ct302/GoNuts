655e4f2a17c585235c3f8be94198e1d7f95b2a5a
Charley Turner <headshotsvol7@gmail.com>
2017-09-18 18:24:48 -0400

    Importing Project

---
+++ goNuts/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace goNuts
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            myFrame.Navigate(typeof(donutPage));
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            myFrame.Navigate(typeof(coffeePage));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            myFrame.Navigate(typeof(donutPage));
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            myFrame.Navigate(typeof(schedulePage));
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            myFrame.Navigate(typeof(completePage));
+        }
+    }
+}
---
+++ goNuts/coffeePage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace goNuts
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class coffeePage : Page
+    {
+        public coffeePage()
+        {
+            this.InitializeComponent();
+        }
+        private string _roast;
+        private string _sweetener;
+        private string _cream;
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var selected = (MenuFlyoutItem)sender;
+            _roast = selected.Text;
+            showResult();
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var selected = (MenuFlyoutItem)sender;
+            _sweetener = selected.Text;
+            showResult();
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            var selected = (MenuFlyoutItem)sender;
+            _cream = selected.Text;
+            showResult();
+        }
+
+        private void showResult()
+        {
+            //showResultTextBlock.Text =  _roast  + _sweetener + _cream;
+            if (_roast == "None" || string.IsNullOrEmpty(_roast))
+            {
+                showResultTextBlock.Text = "None";
+                return;
+            }
+            showResultTextBlock.Text = _roast;
+            if (_cream != "None" && !String.IsNullOrEmpty(_cream))
+            {
+                showResultTextBlock.Text += " + " + _cream;
+            }
+            if (_sweetener != "None" && !String.IsNullOrEmpty(_sweetener))
+            {
+                showResultTextBlock.Text += " + " + _sweetener;
+            }
+
+
+
+        }
+
+    }
+
+}
